c4643dca04c94095e5e345fc23fd1a7fb766ac1c
James Leo <james@jamesleo.co.uk>
2017-12-08 10:29:35 +0800

    Update

---
+++ Prism/Controllers/Grades1Controller.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Prism.Models;
+
+namespace Prism.Controllers
+{
+    public class Grades1Controller : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Grades1
+        public async Task<ActionResult> Index()
+        {
+            return View(await db.Grades.ToListAsync());
+        }
+
+        // GET: Grades1/Details/5
+        public async Task<ActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Grade grade = await db.Grades.FindAsync(id);
+            if (grade == null)
+            {
+                return HttpNotFound();
+            }
+            return View(grade);
+        }
+
+        // GET: Grades1/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Grades1/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include = "GradeId,Level,Name")] Grade grade)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Grades.Add(grade);
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            return View(grade);
+        }
+
+        // GET: Grades1/Edit/5
+        public async Task<ActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Grade grade = await db.Grades.FindAsync(id);
+            if (grade == null)
+            {
+                return HttpNotFound();
+            }
+            return View(grade);
+        }
+
+        // POST: Grades1/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "GradeId,Level,Name")] Grade grade)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(grade).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(grade);
+        }
+
+        // GET: Grades1/Delete/5
+        public async Task<ActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Grade grade = await db.Grades.FindAsync(id);
+            if (grade == null)
+            {
+                return HttpNotFound();
+            }
+            return View(grade);
+        }
+
+        // POST: Grades1/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(int id)
+        {
+            Grade grade = await db.Grades.FindAsync(id);
+            db.Grades.Remove(grade);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
